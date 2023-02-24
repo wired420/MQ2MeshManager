d532fc72420d0841d2978798adda42cc7af7cb32
Clayton Johnson <wired420@gmail.com>
2023-02-24 15:37:22 -0500

    Moved prototypes up where they should be. My bad. Was playing with VS

--- MQ2MeshManager.cpp
+++ MQ2MeshManager.cpp
@@ -49,6 +49,36 @@ PreSetup("MQ2MeshManager");
 PLUGIN_VERSION(2.1);
 
 /**
+ * PROTOTYPES - So we can call these functions in any order without
+ *              playing jigsaw puzzle with our code.
+ */
+void MeshLoadDatabase();
+void MeshWriteChat(const std::string& msg, bool AntiSpam);
+void MeshManager(SPAWNINFO* pChar, char* szLine);
+void MeshDownloadFile(const std::string& url, const std::string& filename, const std::string& savepath, bool single);
+void MeshManagerMenu(const std::string& menu);
+void MeshUpdateDatabase();
+void MeshManagerDisplayHashes(const char* Param2);
+void MeshManagerUpdateZone(const char* Param2);
+void MeshManagerUpdateAll(const char* Param2, const char* Param3);
+void MeshManagerSaveSettings();
+void MeshManagerLoadSettings();
+void MeshManagerSaveIgnores();
+void MeshManagerLoadIgnores();
+void MeshManagerIgnore(const std::string& Param2, const std::string& Param3);
+void Get_Hash_For_Update(const struct HashListStorage& tmp);
+bool move_single_file(std::filesystem::path& source, std::filesystem::path& destination);
+int move_multiple_files(std::filesystem::path source, std::filesystem::path destination, std::vector<std::string> extensions, std::vector<std::string> excludes);
+int number_of_files_in_directory(std::filesystem::path path, std::vector<std::string> extension);
+std::string Get_Hash(const fs::path& p, const std::string& h);
+std::string MD5(const std::string& data);
+std::string SHA256(const std::string& data);
+bool InGameAndSpawned();
+bool ValidateZoneShortName(const std::string& shortname);
+struct DownloadListStorage;
+struct HashListStorage;
+
+/**
  * GLOBALS
  *
  * Avoid Globals if at all possible, since they persist throughout your program.
@@ -126,36 +156,6 @@ struct ProgressMeter {
 	std::string FileUrl;
 	std::string FileName;
 };
-
-/**
- * PROTOTYPES - So we can call these functions in any order without 
- *              playing jigsaw puzzle with our code.
- */
-void MeshLoadDatabase();
-void MeshWriteChat(const std::string& msg, bool AntiSpam);
-void MeshManager(SPAWNINFO* pChar, char* szLine);
-void MeshDownloadFile(const std::string& url, const std::string& filename, const std::string& savepath, bool single);
-void MeshManagerMenu(const std::string& menu);
-void MeshUpdateDatabase();
-void MeshManagerDisplayHashes(const char* Param2);
-void MeshManagerUpdateZone(const char* Param2);
-void MeshManagerUpdateAll(const char* Param2, const char* Param3);
-void MeshManagerSaveSettings();
-void MeshManagerLoadSettings();
-void MeshManagerSaveIgnores();
-void MeshManagerLoadIgnores();
-void MeshManagerIgnore(const std::string& Param2, const std::string& Param3);
-void Get_Hash_For_Update(const struct HashListStorage& tmp);
-bool move_single_file(std::filesystem::path& source, std::filesystem::path& destination);
-int move_multiple_files(std::filesystem::path source, std::filesystem::path destination, std::vector<std::string> extensions, std::vector<std::string> excludes);
-int number_of_files_in_directory(std::filesystem::path path, std::vector<std::string> extension);
-std::string Get_Hash(const fs::path& p, const std::string& h);
-std::string MD5(const std::string& data);
-std::string SHA256(const std::string& data);
-bool InGameAndSpawned();
-bool ValidateZoneShortName(const std::string& shortname);
-struct DownloadListStorage;
-struct HashListStorage;
 
 /**
  * HELPER FUNCTIONS - vcpkg install cryptopp
