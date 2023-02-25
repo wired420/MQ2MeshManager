f2954446ba9e5639dc2428b9461fa41838e07f3c
Clayton Johnson <wired420@gmail.com>
2023-02-24 21:01:13 -0500

    fix: fixed strerrno calls to strerrno_s then got rid of them completely

--- MQ2MeshManager.cpp
+++ MQ2MeshManager.cpp
@@ -211,12 +211,9 @@ void Get_Hash_For_Update(const struct HashListStorage& tmp)
 		{
 			CryptoPP::FileSource file(theFile.c_str(), true, new CryptoPP::StringSink(str));
 		}
-		catch (CryptoPP::FileStore::OpenErr const&)
+		catch (CryptoPP::FileStore::OpenErr const& e)
 		{
-			size_t errmsglen = strerrorlen_s(errno) + 1;
-			char* errmsg = nullptr;
-			strerror_s(errmsg, errmsglen, errno);
-			MeshWriteChat(fmt::format("\arHashing Error:\aw {}", errmsg), false);
+			MeshWriteChat(fmt::format("\arHashing Error:\aw {}", e.GetWhat()), false);
 		}
 
 		if (tmp.HashType == "md5")
@@ -1942,73 +1939,5 @@ PLUGIN_API void OnUpdateImGui()
 			}
 			ImGui::End();
 		}
-	}
-}
-
-// Safe C Library
-// 
-// Copyright (C) 2012, 2013 Cisco Systems
-// Copyright (C) 2017 Reini Urban
-// All rights reserved.
-// 
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-
-size_t strerrorlen_s(errno_t errnum)
-{
-#ifndef ESNULLP
-#define ESNULLP         ( 400 )       /* null ptr                    */
-#endif
-
-#ifndef ESLEWRNG
-#define ESLEWRNG        ( 410 )       /* wrong size                */
-#endif
-
-#ifndef ESLAST
-#define ESLAST ESLEWRNG
-#endif
-
-	static const int len_errmsgs_s[] = {
-	  sizeof "null ptr",               /* ESNULLP */
-	  sizeof "length is zero",         /* ESZEROL */
-	  sizeof "length is below min",    /* ESLEMIN */
-	  sizeof "length exceeds RSIZE_MAX",/* ESLEMAX */
-	  sizeof "overlap undefined",      /* ESOVRLP */
-	  sizeof "empty string",           /* ESEMPTY */
-	  sizeof "not enough space",       /* ESNOSPC */
-	  sizeof "unterminated string",    /* ESUNTERM */
-	  sizeof "no difference",          /* ESNODIFF */
-	  sizeof "not found",              /* ESNOTFND */
-	  sizeof "wrong size",             /* ESLEWRNG */
-	};
-
-	if (errnum >= ESNULLP && errnum <= ESLAST)
-	{
-		return len_errmsgs_s[errnum - ESNULLP] - 1;
-	}
-	else
-	{
-		size_t errmsglen = strerrorlen_s(errnum) + 1;
-		char* errmsg = nullptr;
-		strerror_s(errmsg, errmsglen, errnum);
-		return errmsg ? strlen(errmsg) : 0;
 	}
 }
